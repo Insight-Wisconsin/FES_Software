22805eec7793734d124a456f7737ed25e3ad8708
Mayank <mjain53@wisc.edu>
2025-03-05 15:22:49 -0600

    learned a bit more about the activity_main.xml. It is what is incharge of the layout. When you create a button or text editor, the variable will be availible to you in your MainActivity.java. So Ideally We create the interface of the app first with all the buttons we are going to need and then use those buttons as variables in our acutal backend code.

--- app/src/main/java/com/example/fes_software/MainActivity.java
+++ app/src/main/java/com/example/fes_software/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.fes_software;
 
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -13,12 +16,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button button = findViewById(R.id.button1);
+        EditText editText1 = findViewById(R.id.editText1);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String text = editText1.getText().toString();
+
+                Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
+
+            }
         });
+
     }
 }
