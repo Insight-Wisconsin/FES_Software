f4041ce2d63195b9239c5bdae6ca35af226ed7e3
Mayank <mjain53@wisc.edu>
2025-03-06 13:39:01 -0600

    Added a textview in the middle of the app that takes the input of the editText and shows it. The textview will eventually be replaced with the gyroscope sensor output so that we can see the numbers in real time.

--- app/src/main/java/com/example/fes_software/MainActivity.java
+++ app/src/main/java/com/example/fes_software/MainActivity.java
@@ -5,6 +5,7 @@ import android.widget.Button;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -17,15 +18,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView GyroNumber = (TextView) findViewById(R.id.textView2);
 
         Button button = findViewById(R.id.button1);
         EditText editText1 = findViewById(R.id.editText1);
-
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 String text = editText1.getText().toString();
-
+                GyroNumber.setText(text);
                 Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
 
             }
